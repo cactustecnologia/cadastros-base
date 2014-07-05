0fd832e274391fd0c0b06b987b0c4e718392cfc4
Silvio Barretto <silvioaabarretto@gmail.com>
2014-07-05 08:54:32 -0300

    notebook silvio

--- src/main/java/br/com/cactus/cadastros/model/EstadoCivil.java
+++ src/main/java/br/com/cactus/cadastros/model/EstadoCivil.java
@@ -18,6 +18,7 @@ public class EstadoCivil implements Serializable {
     private static final long serialVersionUID = 1L;
     //teste de atualizacao pelo github
     //teste de atualizacao pelo notebook
+    //teste notebook silvio
     private Integer id;    
     private String nome;    
     private String descricao;
