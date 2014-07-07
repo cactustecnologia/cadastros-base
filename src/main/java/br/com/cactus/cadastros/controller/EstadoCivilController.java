460c6ef6d0c67fe0e00d9b2ae6890b16b05e315b
Silvio Barretto <silvioaabarretto@gmail.com>
2014-07-07 09:04:33 -0300

    criar limpar

--- src/main/java/br/com/cactus/cadastros/controller/EstadoCivilController.java
+++ src/main/java/br/com/cactus/cadastros/controller/EstadoCivilController.java
@@ -2,6 +2,7 @@ package br.com.cactus.cadastros.controller;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -14,13 +15,49 @@ import br.com.cactus.cadastros.repository.EstadoCivis;
 public class EstadoCivilController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@PostConstruct
+	public void init(){
+		limpar();
+	}
+	
 	private EstadoCivil estadoCivil;
 	@Inject
 	private EstadoCivis estadocivis;
 	
-	public void salvar(EstadoCivil estadoCivil){
+	public void limpar(){
+		this.estadoCivil = new EstadoCivil();
+	}
+	
+	public void salvar(){
 		this.estadoCivil = this.estadocivis.salvar(estadoCivil);
+	}
+	
+	public void atualizar(){
+		
+	}
+
+	
+	
+	//getter and setter
+	public EstadoCivil getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(EstadoCivil estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public EstadoCivis getEstadocivis() {
+		return estadocivis;
+	}
+
+	public void setEstadocivis(EstadoCivis estadocivis) {
+		this.estadocivis = estadocivis;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 	
 	
