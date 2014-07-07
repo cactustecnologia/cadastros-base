4303f7334d0d67c0465a8b55830a3607a0f9e717
Silvio Barretto <silvioaabarretto@gmail.com>
2014-07-07 08:57:51 -0300

    controller EstadoCivil / método salvar

--- src/main/java/br/com/cactus/cadastros/controller/EstadoCivilController.java
+++ src/main/java/br/com/cactus/cadastros/controller/EstadoCivilController.java
@@ -1,5 +1,27 @@
 package br.com.cactus.cadastros.controller;
 
-public class EstadoCivilController {
+import java.io.Serializable;
 
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.cactus.cadastros.model.EstadoCivil;
+import br.com.cactus.cadastros.repository.EstadoCivis;
+
+@Named
+@ViewScoped
+public class EstadoCivilController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private EstadoCivil estadoCivil;
+	@Inject
+	private EstadoCivis estadocivis;
+	
+	public void salvar(EstadoCivil estadoCivil){
+		this.estadoCivil = this.estadocivis.salvar(estadoCivil);
+	}
+	
+	
 }
