9562d39397ff48483addf4bf327c63ff024d16d2
Silvio Barretto <silvioaabarretto@gmail.com>
2014-07-07 09:49:20 -0300

    EstadoCivil

--- src/main/java/br/com/cactus/cadastros/controller/EstadoCivilBean.java
+++ src/main/java/br/com/cactus/cadastros/controller/EstadoCivilBean.java
@@ -9,10 +9,12 @@ import javax.inject.Named;
 
 import br.com.cactus.cadastros.model.EstadoCivil;
 import br.com.cactus.cadastros.repository.EstadoCivis;
+import br.com.cactus.cadastros.util.jpa.Transactional;
+import br.com.cactus.cadastros.util.jsf.FacesUtil;
 
 @Named
 @ViewScoped
-public class EstadoCivilController implements Serializable {
+public class EstadoCivilBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -29,14 +31,13 @@ public class EstadoCivilController implements Serializable {
 		this.estadoCivil = new EstadoCivil();
 	}
 	
+	@Transactional
 	public void salvar(){
 		this.estadoCivil = this.estadocivis.salvar(estadoCivil);
+		limpar();
+		FacesUtil.addInfoMessage("Estado Civil salvo com sucesso!");
 	}
 	
-	public void atualizar(){
-		
-	}
-
 	
 	
 	//getter and setter
@@ -47,18 +48,5 @@ public class EstadoCivilController implements Serializable {
 	public void setEstadoCivil(EstadoCivil estadoCivil) {
 		this.estadoCivil = estadoCivil;
 	}
-
-	public EstadoCivis getEstadocivis() {
-		return estadocivis;
-	}
-
-	public void setEstadocivis(EstadoCivis estadocivis) {
-		this.estadocivis = estadocivis;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	
 	
 }
