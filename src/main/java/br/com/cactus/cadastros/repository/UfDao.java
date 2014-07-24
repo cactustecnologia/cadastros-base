bc6bb8723806af759ef9e0a3150c42e392298fec
Silvio Barretto <silvioaabarretto@gmail.com>
2014-07-24 09:57:44 -0300

    classe faltando

---
+++ src/main/java/br/com/cactus/cadastros/repository/UfDao.java
@@ -0,0 +1,32 @@
+package br.com.cactus.cadastros.repository;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.NoResultException;
+
+import br.com.cactus.cadastros.model.Uf;
+
+public class UfDao extends GenericRepositoryDaoImpl<Uf> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Uf guardar(Uf uf){
+		if(uf.getId() == null){
+			return this.salvar(uf);
+		}else{
+			return this.atualizar(uf);
+		}
+	}
+	
+	public Uf porNome(String nome){
+		try{
+			Map<String, Object> params = new HashMap<String, Object>();
+			params.put("nome", nome);
+			return pesqParam("from uf where upper(nome) = :nome", params);
+		} catch (NoResultException ex){
+			return null;
+		}
+	}	
+}
